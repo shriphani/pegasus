22dae74a338cfc0c01709f48c7d68f95c52eca16
Shriphani Palakodety <shriphanip@gmail.com>
2015-09-30 09:54:22 -0700

    core ideas of a process emerging here

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject pegasus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A scaleable production-ready crawler in clojure"
+  :url "http://github.com/shriphani/pegasus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
--- src/pegasus/core.clj
+++ src/pegasus/core.clj
@@ -1,6 +1,19 @@
-(ns pegasus.core)
+(ns pegasus.core
+  (:require [clojure.core.async :as async :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Frontier - handles the to-visit queue.
+;; Extractor - given a web-page, extract the next batch
+;;; of links to download
+;; Writer - Write a payload with other info to disk
+
+;; Each process consumes from a channel and produces into
+;;; a channel. A process obtains a record from its input
+;;; channel, runs the associated routine on this record
+;;; and sends in another record through the output channel
+;;; Park these threads when they're waiting for a response
+(def processes [:frontier :extractor :writer])
+
+(defn crawl
+  "Main crawl method. Use this to spawn a new job"
+  [options]
+  options)
