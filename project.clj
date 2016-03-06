5ff3113a11e2dd34019e52948ea9a360605d1775
Shriphani Palakodety <shriphanip@gmail.com>
2016-03-06 01:32:29 -0800

    updating lib versions

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject pegasus "0.2.3"
+(defproject pegasus "0.3.0"
   :description "A scaleable production-ready crawler in clojure"
   :url "http://github.com/shriphani/pegasus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-http "2.0.1"]
+  :dependencies [[clj-http "2.1.0"]
                  [clj-robots "0.6.0"]
                  [clj-time "0.11.0"]
-                 [com.taoensso/timbre "4.2.1"]
+                 [com.taoensso/timbre "4.3.1"]
                  [enlive "1.1.6"]
                  [com.github.kyleburton/clj-xpath "1.4.5"]
                  [factual/durable-queue "0.1.5"]
@@ -16,5 +16,5 @@
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [prismatic/schema "1.0.4"]]
+                 [prismatic/schema "1.0.5"]]
   :plugins [[lein-ancient "0.6.8"]])
