e2f3f4dfb3a08c85151da27920569930ba517389
Shriphani Palakodety <shriphanip@gmail.com>
2016-03-06 01:17:10 -0800

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pegasus "0.2.2"
+(defproject pegasus "0.2.3"
   :description "A scaleable production-ready crawler in clojure"
   :url "http://github.com/shriphani/pegasus"
   :license {:name "Eclipse Public License"
