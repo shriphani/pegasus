a1fbe30db67cc40ad3def509f2f9d8012de2e09a
dhruvbhatia <dhruv@dhruvbhatia.com>
2016-04-18 22:18:28 +1000

    Update deps.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/shriphani/pegasus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-http "2.1.0"]
+  :dependencies [[clj-http "3.0.1"]
                  [clj-robots "0.6.0"]
                  [clj-time "0.11.0"]
                  [com.taoensso/timbre "4.3.1"]
@@ -16,5 +16,5 @@
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [prismatic/schema "1.0.5"]]
+                 [prismatic/schema "1.1.0"]]
   :plugins [[lein-ancient "0.6.8"]])
