6d913178b2dbfeea6812da5b439c4c696a77fc47
Shriphani Palakodety <shriphanip@gmail.com>
2016-04-18 19:49:50 -0700

    nuked the ccf for jcs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pegasus "0.3.0"
+(defproject pegasus "0.3.1"
   :description "A scaleable production-ready crawler in clojure"
   :url "http://github.com/shriphani/pegasus"
   :license {:name "Eclipse Public License"
@@ -11,7 +11,6 @@
                  [com.github.kyleburton/clj-xpath "1.4.5"]
                  [factual/durable-queue "0.1.5"]
                  [me.raynes/fs "1.4.6"]
-                 [org.apache.commons/commons-jcs-core "2.0-beta-1"]
                  [org.bovinegenius/exploding-fish "0.3.4"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
