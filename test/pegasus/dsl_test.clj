6d921f79797d79009a61bea957f93c8345951eda
Shriphani Palakodety <shriphanip@gmail.com>
2016-11-15 12:50:21 -0800

    Added a DSL test!

---
+++ test/pegasus/dsl_test.clj
@@ -0,0 +1,50 @@
+(ns pegasus.dsl-test
+  (:require [clj-http.client :as client]
+            [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [me.raynes.fs :as fs]
+            [pegasus.core :refer :all]
+            [pegasus.core-test :refer [mock-bodies
+                                       *current-dir*
+                                       job-dir-create-delete-fixture
+                                       all-unique?]]
+            [pegasus.defaults :as defaults]
+            [pegasus.dsl :refer :all]
+            [pegasus.utils :as utils]
+            [taoensso.timbre :as timbre
+             :refer (info)]))
+
+(use-fixtures :each job-dir-create-delete-fixture)
+
+(deftest test-dsl
+  (testing "Test the output of the DSL"
+    (with-redefs [defaults/get-request (fn
+                                         ([& args]
+                                          (->> args
+                                               first
+                                               (get mock-bodies))))]
+      (let [final-config (crawl {:seeds ["http://foo.com/1"]
+                                 :impolite? true
+                                 :user-agent "Hello!!!"
+                                 :job-dir *current-dir*
+                                 :corpus-size 5
+                                 :min-delay-ms 0
+                                 :extractor
+                                 (defextractors
+                                   (extract :at-selector [:a]
+                                            :follow :href))})]
+        (loop []
+          
+          (let [stop (:stop?
+                      @(:state final-config))]
+
+            (if stop
+              (do (is
+                   (= (:num-visited
+                       @(:state final-config))
+                      5))
+                  (is
+                   (all-unique?
+                    (io/file
+                     (:corpus-dir final-config)))))
+              (recur))))))))
